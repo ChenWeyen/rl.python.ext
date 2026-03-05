59faa54cdd411fde65ee7b281eb92db53ced8b73
Weien Chen <80822379+ChenWeyen@users.noreply.github.com>
2026-03-05 09:03:30 +0800

    Add UDP listener controls to extension UI

--- rl/python_ui_ext/tests/test_hello_world.py
+++ rl/python_ui_ext/tests/test_hello_world.py
@@ -8,55 +8,28 @@
 # without an express license agreement from NVIDIA CORPORATION or
 # its affiliates is strictly prohibited.
 
-# NOTE:
-#   omni.kit.test - std python's unittest module with additional wrapping to add
-#   suport for async/await tests
-#   For most things refer to unittest docs:
-#   https://docs.python.org/3/library/unittest.html
 import omni.kit.test
-
-# Extension for writing UI tests (to simulate UI interaction)
 import omni.kit.ui_test as ui_test
 
-# Import extension python module we are testing with absolute import path,
-# as if we are external user (other extension)
 import rl.python_ui_ext
 
 
-# Having a test class dervived from omni.kit.test.AsyncTestCase declared on the
-# root of module will make it auto-discoverable by omni.kit.test
 class Test(omni.kit.test.AsyncTestCase):
-    # Before running each test
     async def setUp(self):
         pass
 
-    # After running each test
     async def tearDown(self):
         pass
 
-    # Actual test, notice it is an "async" function, so "await" can be used if needed
     async def test_hello_public_function(self):
         result = rl.python_ui_ext.some_public_function(4)
         self.assertEqual(result, 256)
 
-    async def test_window_button(self):
-        # Find a label in our window
-        label = ui_test.find("RL Python UI Extension//Frame/**/Label[*]")
+    async def test_udp_ui_defaults(self):
+        status_label = ui_test.find("RL Python UI Extension//Frame/**/Label[*].text=='UDP listener stopped'")
+        count_label = ui_test.find("RL Python UI Extension//Frame/**/Label[*].text=='UDP packets received: 0'")
+        reset_button = ui_test.find("RL Python UI Extension//Frame/**/Button[*].text=='Reset'")
 
-        # Find buttons in our window
-        add_button = ui_test.find(
-            "RL Python UI Extension//Frame/**/Button[*].text=='Add'"
-        )
-        reset_button = ui_test.find(
-            "RL Python UI Extension//Frame/**/Button[*].text=='Reset'"
-        )
-
-        # Click reset button
-        await reset_button.click()
-        self.assertEqual(label.widget.text, "empty")
-
-        await add_button.click()
-        self.assertEqual(label.widget.text, "count: 1")
-
-        await add_button.click()
-        self.assertEqual(label.widget.text, "count: 2")
+        self.assertIsNotNone(status_label)
+        self.assertIsNotNone(count_label)
+        self.assertIsNotNone(reset_button)
